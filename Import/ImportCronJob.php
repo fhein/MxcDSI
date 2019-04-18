c59c7d642e53505a8f8456d32e14cb1be44e4d99
mxcSattler <katrin.sattler@maxence.de>
2019-04-18 14:24:45 +0200

    Cronjob for import created

---
+++ Import/ImportCronJob.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MxcDropshipInnocigs\Import;
+
+use Enlight\Event\SubscriberInterface;
+use Mxc\Shopware\Plugin\Service\ServicesTrait;
+
+class ImportCronJob implements SubscriberInterface
+{
+    use ServicesTrait;
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Shopware_CronJob_MxcDsiImport' => 'onImportCronJob'
+        ];
+    }
+
+    public function onImportCronJob($job)
+    {
+            $this->getServices();
+            $this->services->get(ImportClient::class)->import();
+
+            return True;
+    }
+}
